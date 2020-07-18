a785f95b91d64a7c6a0ac085f88a9f038c77bccd
Drew Gibbons <drewcgibbons@gmail.com>
2020-07-17 20:55:31 -0500

    [Drew] Initial commit

---
+++ matrixsolved/src/main/java/com/matrix_solved/dcg/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.matrix_solved.dcg;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
